209ea1140fdf2eec5ad94dde867fb4b9f856d1e4
Sanchit Ingale <sanchitingale09@gmail.com>
2023-01-31 07:36:05 +0530

    login register done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Register from "./components/Register";
+import Login from "./components/Login";
 import "./style.scss"
 function App() {
   return (
     <div>
-      <Register/>
+      <Login/>
     </div>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Add from "../img/upload.png"
 
-const Login = () => {
+const Register = () => {
   return (
-    <div>
-      
+    <div className='formContainer'>
+        <div className='formWrapper'>
+            <span className='logo'>Fake Chat</span>
+            <span className='title'>Register</span>
+            <form>
+                <input type='email' placeholder='Email'/>
+                <input type='password' placeholder='Password'/>
+                <button>Sign-In</button>
+            </form>
+            <p>You don't have an account ? Register</p>
+        </div>
     </div>
   )
 }
 
-export default Login
+export default Register
